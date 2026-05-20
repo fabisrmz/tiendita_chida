f68448f7ef70825d14d51897201af270db8c462a
fabisrmz <fabioligua0208@gmail.com>
2026-05-20 08:56:18 -0600

    feat(admin):"Dash"

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,7 +3,44 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   imports: [],
-  template: `<p>home works!</p>`,
+  template: `
+    <div class="container">
+
+      <nav class="navbar">
+        <h1 class="logo">Lupidy</h1>
+
+        <ul class="menu">
+          <li>Inicio</li>
+          <li>Servicios</li>
+          <li>Acerca</li>
+          <li>Contacto</li>
+        </ul>
+      </nav>
+
+      <section class="hero">
+
+        <div class="content">
+          <h2>Bienvenido a Lupidy</h2>
+
+          <p>
+            Una aplicación moderna diseñada para ayudarte
+            a conectar tecnología, innovación y bienestar.
+          </p>
+
+          <button>Comenzar</button>
+        </div>
+
+        <div class="image">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/4712/4712027.png"
+            alt="Inicio"
+          />
+        </div>
+
+      </section>
+
+    </div>
+  `,
   styleUrl: './home.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
