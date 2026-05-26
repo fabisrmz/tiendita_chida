03ed403afe5b7eb886748fcb799a15d70c8f6b01
Brawnie <braulioyaelvargas@gamil.com>
2026-05-26 08:33:21 -0600

    neriiiiii

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
+import { Products } from './pages/home/products/products';
 import { Home } from './pages/home/home';
 
+
 export const routes: Routes = [
+{
+  component: Products,
+},
+
    {
     path: '',
     component: Home
   }
+  
 ];
