b67fbb9815e46dc30f90f4675eb0d35c42f289aa
carnita21 <nerilazaroantonio354@gmail.com>
2026-05-27 09:36:33 -0600

    feat(neri): se realizo el registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Products } from './pages/products/products';
+import { Register } from './pages/register/register/register';
 
 export const routes: Routes = [
    {
     path: '',
-    component: Home
+    component: Register,
   },
  {
   path: 'products',
   component: Products
+},{
+  path: 'home',
+  component: Home
 }
+
 ];
